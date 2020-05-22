602ad000c56efb12e938c3dab2607f003334a34e
Deteriorator <liangz.org@gmail.com>
2020-05-22 11:19:18 +0800

    feat(form): redirect and user session

--- apps/__init__.py
+++ apps/__init__.py
@@ -11,7 +11,7 @@
 -------------------------------------------------
 """
 
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask_mail import Mail
 from flask_moment import Moment
@@ -66,15 +66,14 @@ from datetime import datetime
 # @main.route('/')
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    name = None
     form = NameForm()
     if form.validate_on_submit():
-        name = form.name.data
-        form.name.data = ''
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
     return render_template(
         'index.html',
         form=form,
-        name=name,
+        name=session.get('name'),
         current_time=datetime.utcnow()
     )
 
