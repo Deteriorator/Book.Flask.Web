31b30bada91562266bdadd72b3ebddbebebf43ab
Deteriorator <liangz.org@gmail.com>
2020-05-25 09:01:03 +0800

    feat(profile): edit user profile but some problem on moment

--- apps/main/forms.py
+++ apps/main/forms.py
@@ -7,17 +7,24 @@
    Date：          2018/11/8
 -------------------------------------------------
    Change Activity:
-                   2018/11/8:
+                   2020/5/24:
 -------------------------------------------------
 """
 
 
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, Length
 
 
 class NameForm(FlaskForm):
     # 新版本
     name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+
+class EditProfileForm(FlaskForm):
+    name = StringField('Real name', validators=[Length(0, 64)])
+    location = StringField('Location', validators=[Length(0, 64)])
+    about_me = TextAreaField('About me')
     submit = SubmitField('Submit')
