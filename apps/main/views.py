a5c06f8797693a7b353a71a1e7f5c2aa197be037
Deteriorator <liangz.org@gmail.com>
2020-05-26 14:16:29 +0800

    feat(post): blog post pagination

--- apps/main/views.py
+++ apps/main/views.py
@@ -11,7 +11,7 @@
 -------------------------------------------------
 """
 
-from flask import render_template, redirect, url_for, abort, flash
+from flask import render_template, redirect, url_for, abort, flash, request, current_app
 from flask_login import login_required, current_user
 from ..dacorators import admin_required
 from .. import db
@@ -28,8 +28,20 @@ def index():
                     author=current_user._get_current_object())
         db.session.add(post)
         return redirect(url_for('.index'))
-    posts = Post.query.order_by(Post.timestamp.desc()).all()
-    return render_template('index.html', form=form, posts=posts)
+    page = request.args.get('page', 1, type=int)
+    pagination = Post.query.order_by(Post.timestamp.desc()).paginate(
+        page,
+        per_page=current_app.config['FLASK_POSTS_PER_PAGE'],
+        error_out=False
+    )
+    posts = pagination.items
+
+    return render_template(
+        'index.html',
+        form=form,
+        posts=posts,
+        pagination=pagination
+    )
 
 
 @main.route('/user/<username>')
@@ -37,8 +49,19 @@ def user(username):
     user = User.query.filter_by(username=username).first()
     if user is None:
         abort(404)
-    posts = user.posts.order_by(Post.timestamp.desc()).all()
-    return render_template('user.html', user=user, posts=posts)
+    page = request.args.get('page', 1, type=int)
+    pagination = Post.query.order_by(Post.timestamp.desc()).paginate(
+        page,
+        per_page=current_app.config['FLASK_POSTS_PER_PAGE'],
+        error_out=False
+    )
+    posts = pagination.items
+    return render_template(
+        'user.html',
+        user=user,
+        posts=posts,
+        pagination=pagination
+    )
 
 
 @main.route('/edit-profile', methods=['GET', 'POST'])
--- config.py
+++ config.py
@@ -21,6 +21,7 @@ class Config:
     FLASK_MAIL_SUBJECT_PREFIX = '[Flask]'
     FLASK_MAIL_SENDER = os.environ.get('MAIL_USERNAME')
     FLASK_ADMIN = os.environ.get('FLASK_ADMIN')
+    FLASK_POSTS_PER_PAGE = 20
 
     @staticmethod
     def init_app(app):
