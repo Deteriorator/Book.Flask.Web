1a4371a39781bfa0d4a75febfd34b129a293cdb3
Deteriorator <liangz.org@gmail.com>
2020-05-27 18:07:51 +0800

    feat(post): rich text markdown and bleach

--- apps/models.py
+++ apps/models.py
@@ -17,6 +17,8 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin, AnonymousUserMixin
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from flask import current_app, request
+from markdown import markdown
+import bleach
 
 
 class Permission:
@@ -211,6 +213,7 @@ class Post(db.Model):
     body = db.Column(db.Text)
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    body_html = db.Column(db.Text)
 
     @staticmethod
     def generate_fake(count=100):
@@ -228,6 +231,22 @@ class Post(db.Model):
             )
             db.session.add(p)
             db.session.commit()
+
+    @staticmethod
+    def on_changed_body(target, value, oldvalue, initiator):
+        allowed_tags = [
+            'a', 'abbr', 'acronym', 'b', 'blockquote', 'code', 'em',
+            'i', 'li', 'ol', 'pre', 'strong', 'ul', 'h1', 'h2', 'h3', 'p'
+        ]
+        target.body_html = bleach.linkify(
+            bleach.clean(markdown(value, output_format='html'),
+                         tags=allowed_tags,
+                         strip=True
+                         )
+        )
+
+
+db.event.listen(Post.body, 'set', Post.on_changed_body)
 
 
 class AnonymousUser(AnonymousUserMixin):
