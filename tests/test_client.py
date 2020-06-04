86c745bfebd3eaa813360f4d435c5e49d4b017cc
Deteriorator <liangz.org@gmail.com>
2020-06-04 09:33:36 +0800

    feat(test): unit test with flask test client

---
+++ tests/test_client.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+-------------------------------------------------------------------------------
+   @Name：    test_client.py
+   @Desc:     
+   @Author:   liangz.org@gmail.com
+   @Create:   2020.06.04   9:27
+-------------------------------------------------------------------------------
+   @Change:   2020.06.04
+-------------------------------------------------------------------------------
+"""
+
+
+import unittest
+import re
+from flask import url_for
+from apps import create_app, db
+from apps.models import User, Role
+
+
+class FlaskClientTestCase(unittest.TestCase):
+    def setUp(self) -> None:
+        self.app = create_app('testing')
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+        db.create_all()
+        Role.insert_roles()
+        self.client = self.app.test_client(use_cookies=True)
+
+    def tearDown(self) -> None:
+        db.session.remove()
+        db.drop_all()
+        self.app_context.pop()
+
+    def test_home_page(self):
+        response = self.client.get(url_for('main.index'))
+        self.assertTrue('Stranger' in response.get_data(as_text=True))
+
+    def test_register_and_login(self):
+        # register a new account
+        response = self.client.post(url_for('auth.register'), data={
+            'email': 'john@example.com',
+            'username': 'john',
+            'password': 'cat',
+            'password2': 'cat'
+        })
+        self.assertTrue(response.status_code == 302)
+
+        # login with the new account
+        response = self.client.post(url_for('auth.login'), data={
+            'email': 'john@example.com',
+            'password': 'cat'
+        }, follow_redirects=True)
+        self.assertTrue(re.search(b'Hello,\s+john!', response.data))
+        self.assertTrue(
+            b'You have not confirmed your account yet' in response.data)
+
+        # send a confirmation token
+        user = User.query.filter_by(email='john@example.com').first()
+        token = user.generate_confirmation_token()
+        response = self.client.get(url_for('auth.confirm', token=token),
+                                   follow_redirects=True)
+        self.assertTrue(
+            b'You have confirmed your account' in response.data)
+
+        # log out
+        response = self.client.get(url_for('auth.logout'), follow_redirects=True)
+        self.assertTrue(b'You have been logged out' in response.data)
+
+
+
+
